09870b02ce9f8465ade159ca13d7db14dcb32efc
LeNGuyenWi <lenguyenwi@gmail.com>
2017-03-24 12:43:04 +0100

    first .net rest api

---
+++ WebAPIDemo/WebAPIDemo/Controllers/PCsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPIDemo.Controllers
+{
+    public class PCsController : ApiController
+    {
+        // GET: api/PCs
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Pc1", "Pc2" };
+        }
+
+        // GET: api/PCs/5
+        public string Get(int id)
+        {
+            return "Some Pc";
+        }
+
+        // POST: api/PCs
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/PCs/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/PCs/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
